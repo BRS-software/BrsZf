082881a7329e815326dc2028c83baf8c87b4605f
Tomasz Borys <t.borys@brs-software.pl>
2013-05-15 18:26:25 +0200

    dev

--- src/Brs/Zf/Permissions/Acl/Acl.php
+++ src/Brs/Zf/Permissions/Acl/Acl.php
@@ -1,8 +1,26 @@
 <?php
-namespace Brs\Zf\Acl\Permissions\Acl;
 
-class Acl
+namespace Brs\Zf\Permissions\Acl;
+
+use LogicException;
+use Zend\Permissions\Acl\Acl as ZendAcl;
+
+class Acl extends ZendAcl
 {
+    protected $currentRole;
 
+    public function assert($privilege, $resource = null, $role = null)
+    {
+        if (! $this->hasPermission($privilege, $resource, $role)) {
+            throw new PermissionException(
+                'Current user hasn\'t enough permission'
+            );
+        }
+    }
+
+    public function hasPermission($privilege, $resource = null, $role = null)
+    {
+        if (null === )
+    }
 }
 
